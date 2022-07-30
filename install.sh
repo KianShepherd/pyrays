4f9297969906004d28b9f9dc3cd08a4745e18c6d
Kian Shepherd <kshepherd@kx.com>
2022-07-30 18:44:05 -0400

    Add rust raytracing repo

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 cargo build --release --manifest-path=rays/Cargo.toml
 cp ./rays/target/release/librays.rlib ./src/librays.rlib
+cargo build --release --manifest-path=Rust-Raytracing/Cargo.toml
+cp ./Rust-Raytracing/target/release/libraytracing.rlib ./src/libraytracing.rlib
 python -m pip install -e .
