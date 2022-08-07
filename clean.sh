507989e4233ffcf8ea2eceeb85fbae104f7bc910
Kian Shepherd <kshepherd@kx.com>
2022-08-06 22:56:42 -0400

    Update shell scripts

--- clean.sh
+++ clean.sh
@@ -1,10 +1,18 @@
-rm -rf ./pyrays.egg-info
-rm -rf ./dist/git = "https://github.com/KianShepherd/raytrace-rs"
-rm -rf ./build/
+rm -rf pyrays.egg-info
+rm -rf dist/git = "https://github.com/KianShepherd/raytrace-rs"
+rm -rf build/
 rm -f Cargo.lock
-rm -rf ./target
-rm -f ./src/pyrays/pyrays_rs.cpython-310-x86_64-linux-gnu.so
-rm -f ./src/pyrays/pyrays_rs.cp310-win_amd64.pyd
-rm -f ./src/libraytracing.rlib
-rm -rf ./raytrace-rs/target
+rm -rf target
+rm -f src/pyrays/pyrays_rs.cpython-310-x86_64-linux-gnu.so
+rm -f src/pyrays/pyrays_rs.cp310-win_amd64.pyd
+rm -f src/libraytracing.rlib
+rm -rf raytrace-rs/target
+rm -f .coverage
+rm -rf coverage
+rm -f tests/.coverage
+rm -rf tests/.benchmarks
+rm -rf tests/__pycache__
+rm -rf src/pyrays/__pycache__
+rm -rf .benchmarks
+rm -rf .pytest_cache
 python -m pip uninstall pyrays -y
--- install.sh
+++
@@ -1,6 +0,0 @@
-cd ./raytrace-rs
-git pull
-cd ..
-cargo build --release --lib --manifest-path=raytrace-rs/Cargo.toml
-cp ./raytrace-rs/target/release/libraytracing.rlib ./src/libraytracing.rlib
-python -m pip install -e .
